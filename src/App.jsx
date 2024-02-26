c28a55c1ee5467e8b7524b68fd7f2fe7eaee2dc9
Zardron <zardron@clockworkdelivery.com>
2024-02-26 21:21:53 +0400

    create initial search menu component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import "animate.css";
 import { Button } from "flowbite-react";
 import { PiAirplaneTakeoffFill } from "react-icons/pi";
-import { FaGlobe, FaHotel, FaPassport } from "react-icons/fa";
+import { FaGlobe, FaHotel, FaPassport, FaBus, FaShip } from "react-icons/fa";
 import { RiHotelFill } from "react-icons/ri";
 
 const App = () => {
@@ -31,8 +31,8 @@ const App = () => {
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <div className="bg-white dark:bg-black">
         <div className="container mx-auto h-[236px]  relative">
-          <div className="absolute w-full bg-white dark:bg-black -bottom-10 rounded-2xl shadow-lg dark:shadow-md dark:shadow-white border border-white/75">
-            <div className="flex items-center justify-between p-8">
+          <div className="absolute w-full bg-white dark:bg-black -bottom-10 rounded-2xl shadow-lg dark:shadow-md dark:shadow-white border border-white/75 p-8">
+            <div className="flex items-center justify-between">
               <div className="flex items-center justify-around gap-6">
                 <Button gradientDuoTone={"purpleToBlue"}>
                   <PiAirplaneTakeoffFill size={18} className="mr-1" /> Flights
@@ -50,16 +50,66 @@ const App = () => {
                   <RiHotelFill size={18} className="mr-1" /> Apartments
                 </Button>
                 <Button outline={true} gradientDuoTone={"purpleToBlue"}>
-                  <RiHotelFill size={18} className="mr-1" /> Bus
+                  <FaBus size={18} className="mr-1" /> Bus
                 </Button>
                 <Button outline={true} gradientDuoTone={"purpleToBlue"}>
-                  <RiHotelFill size={18} className="mr-1" /> Cruise
+                  <FaShip size={18} className="mr-1" /> Cruise
                 </Button>
               </div>
               <div className="flex items-center gap-6">
                 <a href="">One Way</a>
                 <a href="">Roundtrip</a>
                 <a href="">Multi City</a>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-8 mt-4">
+              <div className="flex flex-row items-center justify-between gap-8 w-full">
+                <div className="w-full bg-[#8b3eea17] p-4 rounded-2xl">
+                  <p className="font-secondary text-sm text-gray-400">
+                    Passenger, Class
+                  </p>
+                  <p className="font-primary text-2xl font-semibold">
+                    0 Passenger
+                  </p>
+                  <p className="font-secondary text-xs font-semibold">
+                    Business
+                  </p>
+                </div>
+                <div className="w-full bg-[#8b3eea17] p-4 rounded-2xl">
+                  <p className="font-secondary text-sm text-gray-400">
+                    Passenger, Class
+                  </p>
+                  <p className="font-primary text-2xl font-semibold">
+                    0 Passenger
+                  </p>
+                  <p className="font-secondary text-xs font-semibold">
+                    Business
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between gap-8 w-full">
+                <div className="w-[70%] bg-[#8b3eea17] p-4 rounded-2xl">
+                  <p className="font-secondary text-sm text-gray-400">
+                    Passenger, Class
+                  </p>
+                  <p className="font-primary text-2xl font-semibold">
+                    0 Passenger
+                  </p>
+                  <p className="font-secondary text-xs font-semibold">
+                    Business
+                  </p>
+                </div>
+                <div className="w-[30%] bg-[#8b3eea17] p-4 rounded-2xl">
+                  <p className="font-secondary text-sm text-gray-400">
+                    Passenger, Class
+                  </p>
+                  <p className="font-primary text-2xl font-semibold">
+                    0 Passenger
+                  </p>
+                  <p className="font-secondary text-xs font-semibold">
+                    Business
+                  </p>
+                </div>
               </div>
             </div>
           </div>
